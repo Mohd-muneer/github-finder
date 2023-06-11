5887931ccb5490c276debe1de5b911bfc858d167
Mohd-muneer <mohdmuneeruddin456@gmail.com>
2023-06-12 00:16:04 +0530

    error solved

--- github-finder/postcss.config.js
+++ github-finder/postcss.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
+
+
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
